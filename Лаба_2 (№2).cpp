622b78ae58930520eb265b04b71e47f56637304d
7roman0vna7 <90880098+7roman0vna7@users.noreply.github.com>
2021-10-22 18:25:58 +0300

    Add files via upload

---
+++ Лаба_2 (№2).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibonachi(int x) {
+	if (x == 0)
+		return x;
+	if (x == 1)
+		return x;
+	else
+		return fibonachi(x - 1) + fibonachi(x - 2);
+}
+
+int main() {
+	setlocale(0, "");
+	int x = 0;
+	int s = 0;
+	cout << "Введите число: " << endl;
+	cin >> x;
+	while (s <= x) {
+		cout << fibonachi(s) << endl;
+		s++;
+	}
+	return 0;
+}
